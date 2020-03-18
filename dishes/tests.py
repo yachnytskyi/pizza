d64b107b840bea9ef3fc39448fbfa83061ceed6b
yachnytskyi <34445544+yachnytskyi@users.noreply.github.com>
2020-03-18 15:57:47 +0200

    Wrote the first tests for Ingredient and Dish models

--- dishes/tests.py
+++ dishes/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from dishes.models import Dish, Ingredient
 
-# Create your tests here.
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.ingredient1 = Ingredient.objects.create(
+            title='Ingredient 1'
+        )
+        self.ingredient2 = Ingredient.objects.create(
+            title='Ingredient 2'
+        )
+
+    def test_ingredient_is_assigned_title_on_creation(self):
+        self.assertEquals(self.ingredient1.title, 'Ingredient 1')
+
+    def test_dish_is_assigned_title_on_creation(self):
+        self.dish1 = Dish.objects.create(
+            title="Dish 1"
+        )
+        self.assertEquals(self.dish1.title, 'Dish 1')
+
+    def test_dish_is_assigned_ingredient_title_on_creation(self):
+        self.dish1 = Dish.objects.create(
+            title="Dish 1"
+        )
+        self.dish1.ingredients.add(self.ingredient1)
+        self.assertEquals(list(self.dish1.ingredients.all()), [self.ingredient1])
+
